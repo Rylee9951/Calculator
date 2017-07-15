6cc7c3937367acef3294085f6424d8b15c8663c7
Ryan Lee <djrlee51@gmail.com>
2017-07-14 22:51:24 -0700

    fixed width and organized main.js

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ var decimalAdded = false;
 for(var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(e) {
 		// Get the input and button values
+		//input is now the screen
 		var input = document.querySelector('.screen');
+		//create inputVal to contain everything passed into the screen
 		var inputVal = input.innerHTML;
+		//btnVal is every value passed in except C, =, and multiply
 		var btnVal = this.innerHTML;
 		
 		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
@@ -18,22 +21,18 @@ for(var i = 0; i < keys.length; i++) {
 			input.innerHTML = '';
 			decimalAdded = false;
 		}
-		
 		// If eval key is pressed, calculate and display the result
 		else if(btnVal == '=') {
 			var equation = inputVal;
+			//lastChar is the solution  
 			var lastChar = equation[equation.length - 1];
-			
-			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+			// Replace all instances of x with * respectively. This can be done easily using regex 
 			equation = equation.replace(/X/g, '*');
-			
 			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
 				equation = equation.replace(/.$/, '');
-			
 			if(equation)
 				input.innerHTML = eval(equation);
-				
 			decimalAdded = false;
 		}
 		
@@ -42,31 +41,24 @@ for(var i = 0; i < keys.length; i++) {
 		// 2. The equation shouldn't start from an operator except minus
 		// 3. not more than 1 decimal should be there in a number
 		
-		// We'll fix these issues using some simple checks
-		
 		
 		else if(operators.indexOf(btnVal) > -1) {
 			// Operator is clicked
 			// Get the last character from the equation
 			var lastChar = inputVal[inputVal.length - 1];
-			
-			// Only add operator if input is not empty and there is no operator at the last
+			// Only add operator if input is not empty and there is no operator at the end
 			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
 				input.innerHTML += btnVal;
-			
 			// Allow minus if the string is empty
 			else if(inputVal == '' && btnVal == '-') 
 				input.innerHTML += btnVal;
-			
 			// Replace the last operator (if exists) with the newly pressed operator
 			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
 				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
 				input.innerHTML = inputVal.replace(/.$/, btnVal);
 			}
-			
 			decimalAdded =false;
 		}
-		
 		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
 		else if(btnVal == '.') {
 			if(!decimalAdded) {
